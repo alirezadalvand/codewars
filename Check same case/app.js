8aa2888dfd754c2b3946b8602030bc1e18bc922b
alirezadalvand <alireza.d1339@gmail.com>
2022-05-30 11:47:11 +0430

    1 way

---
+++ Check same case/app.js
@@ -0,0 +1,25 @@
+function sameCase(a, b){
+
+    const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+    const alpha2 = Array.from(Array(26)).map((e, i) => i + 97);
+    
+    const alphabet = alpha.map((x) => String.fromCharCode(x));
+    const alphabet2 = alpha2.map((x) => String.fromCharCode(x));
+    
+    const allLetter = [...alphabet,...alphabet2]
+    
+        if (allLetter.indexOf(a) !== -1 && allLetter.indexOf(b) !== -1) {
+          if (a === a.toLowerCase() && b === b.toLowerCase()||a===a.toUpperCase()&&b===b.toUpperCase()) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+         else if (allLetter.indexOf(a) !== 1 || allLetter.indexOf(b) !== 1) {
+           return -1;
+         }
+    
+    }
+
+
+
