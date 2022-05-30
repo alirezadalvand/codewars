8763e83aa58a1d61b9e394bc0ce7cd87b8203b2f
alirezadalvand <alireza.d1339@gmail.com>
2022-05-30 11:47:25 +0430

    2 way

--- Check same case/app.js
+++ Check same case/app.js
@@ -23,3 +23,33 @@ function sameCase(a, b){
 
 
 
+
+
+
+
+    //////////////////////////////////////////////////////////////////
+
+
+    function sameCase(a, b){
+        if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+           return -1
+         }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+             return 1
+         }else{
+           return 0
+         }
+       }
+
+
+
+
+
+
+
+
+
+
+
+       /////////////////////////////////////////////////
+
+
