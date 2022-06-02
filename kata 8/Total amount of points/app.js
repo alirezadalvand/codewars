b17622e8d992b571fa23af2fb5663c3c62508410
alirezadalvand <alireza.d1339@gmail.com>
2022-06-02 12:55:29 +0430

    kata 8

---
+++ kata 8/Total amount of points/app.js
@@ -0,0 +1,30 @@
+function points(games) {
+    let arr = []
+    let sum = 0
+    games.map((num)=>{
+   if(num[0]>num[2]){
+       sum +=3
+   }else if (num[0] === num[2]) {
+       sum +=1
+   }
+  
+   
+    })
+     return sum;
+  }
+
+
+
+
+  ///////////////////////////////////
+
+
+
+  const points = g => g.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0)
+
+
+
+  /////////////////////////////////////////
+
+
+  const points = games => games.reduce((sum, [x, , y]) => sum + (x > y ? 3 : x == y), 0)
