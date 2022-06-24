97d3f69abb3c52705bc4469b5ed673fa605954eb
alirezadalvand <alireza.d1339@gmail.com>
2022-06-24 16:17:25 +0430

    way2

--- 5/not very secure/app.js
+++ 5/not very secure/app.js
@@ -1,3 +1,16 @@
 function alphanumeric(string) {
     return /^[\da-z0-9]+$/gi.test(string)
   }
+
+
+  /////////////////////////////
+
+
+  const alphanumeric = (str) => {
+    if (!str.length) return false;
+    const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for (let char of str) {
+      if (!ALLOWED_CHARS.includes(char)) return false;
+    }
+    return true;
+  };
