d35bb6f77a4745226594a34d7d4aa9a60a530761
alirezadalvand <alireza.d1339@gmail.com>
2022-06-24 16:12:58 +0430

    way1

---
+++ 5/guess the gifts/app.js
@@ -0,0 +1,14 @@
+
+function guessGifts(wishlist, presents) {
+    return wishlist
+      .filter(whish =>
+        presents.some(
+          present =>
+            present.size === whish.size &&
+            present.clatters === whish.clatters &&
+            present.weight === whish.weight
+        )
+      )
+      .map(whish => whish.name)
+  }
+  
