a953a843d16df07fe509b2ccbc5f1782f8853d29
alirezadalvand <alireza.d1339@gmail.com>
2022-07-09 16:25:22 +0430

    way3

--- 5/ Rot13/app.js
+++ 5/ Rot13/app.js
@@ -12,3 +12,10 @@ function rot13(message) {
 
   ///////////////////////////////////////////////////////////////////
   
+  function rot13(message){
+    return message.replace(/[a-zA-Z]/g, function(c){
+      var c13 = c.charCodeAt(0) + 13;
+      var endCharCode = ( c > 'Z')? 122 : 90;
+      return String.fromCharCode(  endCharCode < c13 ?  c.charCodeAt(0) - 13 : c13 );
+    });
+  }
