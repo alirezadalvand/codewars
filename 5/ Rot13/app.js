9453b48b1a73d30497f5f8fdaefaf2d36892f5e0
alirezadalvand <alireza.d1339@gmail.com>
2022-07-09 16:22:48 +0430

    way1

---
+++ 5/ Rot13/app.js
@@ -0,0 +1,6 @@
+
+function rot13(message) {
+    var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+  }
